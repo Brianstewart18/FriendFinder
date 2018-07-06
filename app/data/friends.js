7b351fe5db6622938a88fd6b30fef4878da4c269
Brian Stewart <brianstewart1912@gmail.com>
2018-07-06 12:25:52 -0500

    msg

---
+++ app/data/friends.js
@@ -0,0 +1,77 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'dummy' data
+
+var friendArray = [{
+        name: "Drax - The destroyer",
+        photo: "https://vignette3.wikia.nocookie.net/marvelcrossroads/images/8/8c/Drax_the_Destroyer_%28Earth-014%29.png/revision/latest?cb=20151115172248",
+        scores: [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    }, {
+        name: "Star Lord",
+        photo: "https://www.sideshowtoy.com/wp-content/uploads/2014/07/902219-product-silo.png",
+        scores: [
+            "4",
+            "2",
+            "5",
+            "1",
+            "3",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2"
+        ]
+
+    }, {
+
+
+        name: "Baby Groot",
+        photo: "https://vignette2.wikia.nocookie.net/disney/images/0/0b/GOTG2_-_Groot.png/revision/latest/scale-to-width-down/515?cb=20170409114426",
+        scores: [
+            "4",
+            "2",
+            "5",
+            "1",
+            "3",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2"
+        ]
+
+    }, {
+
+
+        name: "Gamora",
+        photo: "http://www.findurfuture.com/wp-content/uploads/2017/01/Gamora-Costume.jpg",
+        scores: [
+            "4",
+            "2",
+            "5",
+            "1",
+            "3",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2"
+        ]
+
+    }
+
+
+]
+
+
+module.exports = friendArray;
